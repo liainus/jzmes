4c413be5d1fba84130ae16d8eab86d4436c8f07b
DESKTOP-2CP0C7F\maomao <1765785706@qq.com>
2018-07-10 15:40:06 +0800

    no message

--- Model/BSFramwork.py
+++ Model/BSFramwork.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import DeclarativeMeta
 import time,datetime,decimal
 import json
+from sqlalchemy.orm.collections import InstrumentedList
 
 class AlchemyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -14,7 +15,9 @@ class AlchemyEncoder(json.JSONEncoder):
                     fields[field] = data
                 except TypeError:    # 添加了对datetime的处理
                     # print(type(data),data)
-                    if isinstance(data, datetime.datetime):
+                    if isinstance(data, InstrumentedList):
+                        fields[field] = ''
+                    elif isinstance(data, datetime.datetime):
                         fields[field] = data.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] #SQLserver数据库中毫秒是3位，日期格式;2015-05-12 11:13:58.543
                     elif isinstance(data, datetime.date):
                         fields[field] = data.strftime("%Y-%m-%d")
