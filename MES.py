7600867c96322cf7288d070f05563efa9fb240bf
zhouchangxiang <40376999+zhouchangxiang@users.noreply.github.com>
2018-08-02 10:58:12 +0800

    no message

--- MES.py
+++ MES.py
@@ -1250,6 +1250,33 @@ def ZYPlansFind():
         re = ZYPlanIFS.ZYPlansFind(data)
         return re
 
+#批次管理查询
+@app.route('/batchManager/SearchBatchManager')
+def SearchBatchManager():
+    if request.method == 'GET':
+        data = request.values
+        try:
+            json_str = json.dumps(data.to_dict())
+            if len(json_str) > 10:
+                pages = int(data['page'])  # 页数
+                rowsnumber = int(data['rows'])  # 行数
+                inipage = (pages - 1) * rowsnumber + 0  # 起始页
+                endpage = (pages - 1) * rowsnumber + rowsnumber  # 截止页
+                currentWorkdate = data["currentWorkdate"]
+                if(currentWorkdate == "" or currentWorkdate == None):
+                    total = session.query(ZYPlan).count()
+                    zYPlans = session.query(ZYPlan).order_by("EnterTime desc").all()[inipage:endpage]
+                else:
+                    total = session.query(ZYPlan).filer(ZYPlan.PlanBeginTime == currentWorkdate).count()
+                    zYPlans = session.query(ZYPlan).filer(ZYPlan.PlanBeginTime == currentWorkdate).order_by("PlanBeginTime desc").all()[inipage:endpage]
+                jsonzyplans = json.dumps(zYPlans, cls=AlchemyEncoder, ensure_ascii=False)
+                jsonzyplans = '{"total"' + ":" + str(total) + ',"rows"' + ":\n" + jsonzyplans + "}"
+                return jsonzyplans
+        except Exception as e:
+            print(e)
+            logger.error(e)
+            insertSyslog("error", "批次管理查询报错Error：" + str(e), "AAAAAAadmin")
+            return json.dumps([{"status": "Error:" + str(e)}], cls=AlchemyEncoder, ensure_ascii=False)
 
 # role更新数据，通过传入的json数据，解析之后进行相应更新
 @app.route('/allZYPlans/Update', methods=['POST', 'GET'])
