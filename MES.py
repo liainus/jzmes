8415450923771a86e7333333d4b0089b4c6610a3
DESKTOP-2CP0C7F\maomao <1765785706@qq.com>
2018-08-22 11:17:15 +0800

    no message

--- MES.py
+++ MES.py
@@ -3177,10 +3177,10 @@ def unique_num():
     import datetime
     import random
     nowTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")  # 生成当前时间
-    randomNum = random.randint(0, 100)  # 生成的随机整数n，其中0<=n<=100
-    if randomNum <= 10:
-        randomNum = str(0) + str(randomNum)
-    uniqueNum = str(nowTime) + str(randomNum)
+    # randomNum = random.randint(0, 100)  # 生成的随机整数n，其中0<=n<=100
+    # if randomNum <= 10:
+    #     randomNum = str(0) + str(randomNum)
+    uniqueNum = str(nowTime)
     return uniqueNum
 
 # global id
