8e119fb7d716b3589de21f535ed731943df97eeb
DESKTOP-2CP0C7F\maomao <1765785706@qq.com>
2018-10-12 15:46:01 +0800

    no message

--- MES.py
+++ MES.py
@@ -5407,17 +5407,17 @@ def DataPart():
     return render_template('BatchData_Process_Data.html')
 
 # 过程连续数据——彩虹图
-@app.route('ProcessContinuousData/Rainbow')
+@app.route('/ProcessContinuousData/Rainbow')
 def Rainbow():
     return render_template('BatchData_Process_Trend.html')
 
 # 过程连续数据——CPK
-@app.route('ProcessContinuousData/CPK')
+@app.route('/ProcessContinuousData/CPK')
 def CPK():
     return render_template('BatchData_Process_CPK.html')
 
 # 过程连续数据—— 直方图
-@app.route('ProcessContinuousData/CPK')
+@app.route('/ProcessContinuousData/CPK')
 def histogram():
     return render_template('BatchData_Process_Histogram.html')
 
