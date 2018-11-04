dc9277ef0e66c6cf52a28fadd0f314f21d470262
zhouchangxiang <40376999+zhouchangxiang@users.noreply.github.com>
2018-11-04 17:20:41 +0800

    no message

--- MES.py
+++ MES.py
@@ -5529,6 +5529,37 @@ def electronicBatchRecord():
         QAConfirmPeople_c6 = ""
         OperationPeople_c7 = ""
         CheckedPeople_c7 = ""
+
+        OperationPeople_d1 = ""
+        CheckedPeople_d1 = ""
+        QAConfirmPeople_d1 = ""
+        OperationPeople_d2 = ""
+        CheckedPeople_d2 = ""
+        OperationPeople_d3 = ""
+        CheckedPeople_d3 = ""
+        QAConfirmPeople_d3 = ""
+        OperationPeople_d4 = ""
+        CheckedPeople_d4 = ""
+
+        OperationPeople_e1 = ""
+        CheckedPeople_e1 = ""
+        QAConfirmPeople_e1 = ""
+        OperationPeople_e2 = ""
+        CheckedPeople_e2 = ""
+        QAConfirmPeople_e2 = ""
+        OperationPeople_e3 = ""
+        CheckedPeople_e3 = ""
+        QAConfirmPeople_e3 = ""
+        QAConfirmPeople_e4 = ""
+        OperationPeople_e5 = ""
+        CheckedPeople_e5 = ""
+        OperationPeople_e6 = ""
+        CheckedPeople_e6 = ""
+        QAConfirmPeople_e6 = ""
+        OperationPeople_e7 = ""
+        CheckedPeople_e7 = ""
+        OperationPeople_e8 = ""
+        CheckedPeople_e8 = ""
         if(title == "备料"):
             re = electronicBatchRecords("备料段", oclass.BrandID, oclass.BatchID, ID)
             Pclass = re[0]
@@ -5671,8 +5702,8 @@ def electronicBatchRecord():
             Pclass = re[0]
             Zclass = re[1]
             if (re[2] != None):
-                OperationPeople_e8 = re[2].OperationPeople
-                CheckedPeople_e8 = re[2].CheckedPeople
+                OperationPeople_e7 = re[2].OperationPeople
+                CheckedPeople_e7 = re[2].CheckedPeople
             Noclas = re[3]
             for no in Noclas:
                 if (no.name == "（收粉段）生产前准备（操作人）"):
@@ -5688,22 +5719,27 @@ def electronicBatchRecord():
                 elif (no.name == "收粉开始，操作按SOP执行（QA签名）"):
                     QAConfirmPeople_e3 = no.oddUser
                 elif (no.name == "收粉结束，按SOP清场（操作人）"):
-                    OperationPeople_e7 = no.oddUser
+                    OperationPeople_e6 = no.oddUser
                 elif (no.name == "收粉结束，按SOP清场（复核人）"):
-                    CheckedPeople_e7 = no.oddUser
+                    CheckedPeople_e6 = no.oddUser
                 elif (no.name == "收粉结束，按SOP清场（QA签名）"):
-                    QAConfirmPeople_e7 = no.oddUser
+                    QAConfirmPeople_e6 = no.oddUser
             Newoclasss = db_session.query(NewReadyWork).filter(NewReadyWork.PUID == Pclass.PUID, NewReadyWork.BatchID ==
-                                                               oclass.BatchID, NewReadyWork.Type.in_(("48", "49", "50", "51"))).all()
+                                                               oclass.BatchID, NewReadyWork.Type.in_(("48", "50", "51", "52"))).all()
             if(len(Newoclasss) > 0):
                 for nc in Newoclasss:
                     if(nc.Type == "48"):
                         OperationPeople_e2 = nc.OperationPeople
                         CheckedPeople_e2 = nc.CheckedPeople
                         QAConfirmPeople_e2 = nc.QAConfirmPeople
-                    elif(nc.Type == "49"):
+                    elif(nc.Type == "50"):
+                        OperationPeople_e5 = nc.OperationPeople
+                        CheckedPeople_e5 = nc.CheckedPeople
+                    elif (nc.Type == "51"):
+                        OperationPeople_e8 = nc.OperationPeople
+                        CheckedPeople_e8 = nc.CheckedPeople
+                    elif (nc.Type == "52"):
                         QAConfirmPeople_e4 = nc.QAConfirmPeople
-
         elif (title == "醇沉"):
             re = electronicBatchRecords("醇沉段", oclass.BrandID, oclass.BatchID,ID)
             Pclass = re[0]
@@ -5732,7 +5768,25 @@ def electronicBatchRecord():
                            PName=Pclass.PDUnitRouteName,PUID=Pclass.PUID,BatchID=oclass.BatchID,PlanQuantity=oclass.PlanQuantity,
                            ActBeginTime=Zclass.ActBeginTime,flag=flag,OperationPeople_a1=OperationPeople_a1,CheckedPeople_a1=CheckedPeople_a1,QAConfirmPeople_a1=QAConfirmPeople_a1,
                            OperationPeople_a2=OperationPeople_a2,CheckedPeople_a2=CheckedPeople_a2,OperationPeople_a3=OperationPeople_a3,CheckedPeople_a3=CheckedPeople_a3,
-                           QAConfirmPeople_a3=QAConfirmPeople_a3,OperationPeople_a4=OperationPeople_a4,CheckedPeople_a4=CheckedPeople_a4,QAConfirmPeople_a4=QAConfirmPeople_a4,OperationPeople_a5=OperationPeople_a5,CheckedPeople_a5=CheckedPeople_a5)
+                           QAConfirmPeople_a3=QAConfirmPeople_a3,OperationPeople_a4=OperationPeople_a4,CheckedPeople_a4=CheckedPeople_a4,QAConfirmPeople_a4=QAConfirmPeople_a4,
+                           OperationPeople_a5=OperationPeople_a5,CheckedPeople_a5=CheckedPeople_a5,OperationPeople_b1 = OperationPeople_b1,CheckedPeople_b1 = CheckedPeople_b1,
+                           QAConfirmPeople_b1 = QAConfirmPeople_b1,OperationPeople_b2 = OperationPeople_b2,CheckedPeople_b2 = CheckedPeople_b2,QAConfirmPeople_b2 = QAConfirmPeople_b2,
+                           OperationPeople_b3 = OperationPeople_b3,CheckedPeople_b3 = CheckedPeople_b3,QAConfirmPeople_b3 = QAConfirmPeople_b3,OperationPeople_b4 = OperationPeople_b4,
+                           CheckedPeople_b4 = CheckedPeople_b4,QAConfirmPeople_b4 = QAConfirmPeople_b4,OperationPeople_b5 = OperationPeople_b5,CheckedPeople_b5 = CheckedPeople_b5,
+                           QAConfirmPeople_b5 = QAConfirmPeople_b5,OperationPeople_b6 = OperationPeople_b6,CheckedPeople_b6 = CheckedPeople_b6,
+                           OperationPeople_c1=OperationPeople_c1,CheckedPeople_c1 = CheckedPeople_c1,QAConfirmPeople_c1 = QAConfirmPeople_c1,OperationPeople_c2 = OperationPeople_c2,
+                           CheckedPeople_c2 = CheckedPeople_c2,QAConfirmPeople_c2 = QAConfirmPeople_c2,OperationPeople_c3 = OperationPeople_c3,CheckedPeople_c3 = CheckedPeople_c3,
+                           QAConfirmPeople_c3 = QAConfirmPeople_c3,OperationPeople_c4 = OperationPeople_c4,CheckedPeople_c4 = CheckedPeople_c4,QAConfirmPeople_c4 = QAConfirmPeople_c4,
+                           OperationPeople_c5 = OperationPeople_c5,CheckedPeople_c5 = CheckedPeople_c5,QAConfirmPeople_c5 = QAConfirmPeople_c5,OperationPeople_c6 = OperationPeople_c6,
+                           CheckedPeople_c6 = CheckedPeople_c6,QAConfirmPeople_c6 = QAConfirmPeople_c6,OperationPeople_c7 = OperationPeople_c7,CheckedPeople_c7 = CheckedPeople_c7,
+                           OperationPeople_d1=OperationPeople_d1,CheckedPeople_d1 = CheckedPeople_d1,QAConfirmPeople_d1 = QAConfirmPeople_d1,OperationPeople_d2 = OperationPeople_d2,
+                           CheckedPeople_d2 = CheckedPeople_d2,OperationPeople_d3 = OperationPeople_d3,CheckedPeople_d3 = CheckedPeople_d3,QAConfirmPeople_d3 = QAConfirmPeople_d3,
+                           OperationPeople_d4 = OperationPeople_d4,CheckedPeople_d4 = CheckedPeople_d4,
+                           OperationPeople_e1 = OperationPeople_e1,CheckedPeople_e1 = CheckedPeople_e1,QAConfirmPeople_e1 = QAConfirmPeople_e1,OperationPeople_e2 = OperationPeople_e2,
+                           CheckedPeople_e2 = CheckedPeople_e2,QAConfirmPeople_e2 = QAConfirmPeople_e2,OperationPeople_e3 = OperationPeople_e3,CheckedPeople_e3 = CheckedPeople_e3,
+                           QAConfirmPeople_e3 = QAConfirmPeople_e3,QAConfirmPeople_e4 = QAConfirmPeople_e4,OperationPeople_e5 = OperationPeople_e5,CheckedPeople_e5 = CheckedPeople_e5,
+                           OperationPeople_e6 = OperationPeople_e6,CheckedPeople_e6 = CheckedPeople_e6,QAConfirmPeople_e6 = QAConfirmPeople_e6,OperationPeople_e7 = OperationPeople_e7,
+                           CheckedPeople_e7 = CheckedPeople_e7,OperationPeople_e8 = OperationPeople_e8,CheckedPeople_e8 = CheckedPeople_e8)
 def electronicBatchRecords(name,BrandID,BatchID,ID):
     Pclass = db_session.query(ProductUnitRoute).filter(ProductUnitRoute.PDUnitRouteName == name,
                                                        ProductUnitRoute.ProductRuleID == BrandID).first()
