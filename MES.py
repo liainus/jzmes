5c3a3a23ec4f4905573aab27c1a51e185968047d
DESKTOP-2CP0C7F\maomao <1765785706@qq.com>
2018-12-01 13:18:56 +0800

    no message

--- MES.py
+++ MES.py
@@ -7486,7 +7486,7 @@ def BatchDataCompare():
                 if len(input)>0 and len(output)>0 and len(sampling_quantity)>0:
                     _data['input'] = input
                     _data['output'] = output
-                    _data['yield'] = (float(output)+float(sampling_quantity))/float(input)*100%
+                    _data['yield'] = (float(output)+float(sampling_quantity))/float(input)
                     _data['batch'] = batch
                     data_list.append(_data)
                 return
