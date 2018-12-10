73cccb0f3ee4efafd9a60dad4d0a0c6a483e4dd2
zhouchangxiang <40376999+zhouchangxiang@users.noreply.github.com>
2018-12-10 13:58:35 +0800

    no message

--- MES.py
+++ MES.py
@@ -7598,7 +7598,16 @@ def EquipmentMaintainFinished():
 # 设备维护
 @app.route('/equipmentMaintain')
 def equipmentMaintain():
-    return render_template('equipmentMaintain.html')
+    RoleNames = db_session.query(User.RoleName).filter(User.Name == current_user.Name).all()
+    rolename = ""
+    for name in RoleNames:
+        if name[0] == "设备管理部审核人":
+            rolename = "设备管理部审核人"
+        if name[0] == "设备部技术人员":
+            rolename = "设备部技术人员"
+        if name[0] == "系统管理员":
+            rolename = "系统管理员"
+    return render_template('equipmentMaintain.html', rolename=rolename)
 
 # 设备运行数据
 @app.route('/EquipmentManage/runData')
